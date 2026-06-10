ad508647a3cca3aaa9bcef449e86ed9f608efc1f
udhaykira <2003udhaykiran@gmail.com>
2026-06-10 15:23:02 +0530

    Customized user model for django admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,4 +2,39 @@ from django.contrib import admin
 from .models import User
 
 # Register your models here.
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'created_at',
+        'updated_at',
+        'id',
+        'username',
+        'email',
+        'phone_number',
+        'is_verified',
+        'is_staff'
+    )
+
+    search_fields = (
+        'username',
+        'phone_number',
+        'email'
+    )
+
+    list_filter = (
+        'is_verified',
+        'is_staff',
+        'is_superuser'
+    )
+
+    ordering = (
+        '-created_at',
+    )
+
+    list_per_page = 10
+
+    readonly_fields = (
+        'created_at',
+        'updated_at'
+    )
