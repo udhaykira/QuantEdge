09402bbf9768b7b23c8a43adc06139be8e5b9b21
udhaykira <2003udhaykiran@gmail.com>
2026-06-15 15:43:11 +0530

    Implemented refresh token and logout APIs using Simple JWT

--- accounts/views/__init__.py
+++ accounts/views/__init__.py
@@ -2,3 +2,4 @@ from .register_view import RegisterView
 from .login_view import LoginView
 from .profile_view import ProfileView
 from .change_password_view import ChangePasswordView
+from .logout_view import LogoutView
